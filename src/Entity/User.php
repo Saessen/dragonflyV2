acd2a47aa29771b394448dd0bda8f0d94f76dbcc
Aurelie <aurebes@gmail.com>
2021-05-30 21:10:57 +0200

    Suppression lat lng

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -86,15 +86,6 @@ class User implements UserInterface
      */
     private $pays;
 
-    /**
-     * @ORM\Column(type="float", nullable=true)
-     */
-    private $latitude;
-
-    /**
-     * @ORM\Column(type="float", nullable=true)
-     */
-    private $longitude;
 
     /**
      * @ORM\ManyToOne(targetEntity=Entreprise::class, inversedBy="user")
@@ -319,29 +310,7 @@ class User implements UserInterface
         return $this;
     }
 
-    public function getLatitude(): ?float
-    {
-        return $this->latitude;
-    }
-
-    public function setLatitude(float $latitude): self
-    {
-        $this->latitude = $latitude;
-
-        return $this;
-    }
-
-    public function getLongitude(): ?float
-    {
-        return $this->longitude;
-    }
-
-    public function setLongitude(float $longitude): self
-    {
-        $this->longitude = $longitude;
-
-        return $this;
-    }
+   
 
     public function getEntreprise(): ?Entreprise
     {
