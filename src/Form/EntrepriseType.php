470deac810c2f3398ad65249bb3b6161e5d8bb0d
Lucie Gueguen <lucie.gueguen.57@gmail.com>
2021-06-01 00:45:23 +0200

    Affichages Inscription + Connexion

--- src/Form/EntrepriseType.php
+++ src/Form/EntrepriseType.php
@@ -15,10 +15,10 @@ class EntrepriseType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('siren',TextType::class, array('label'=>'Siren', 'attr'=>array('placeholder'=>'Entrez le numéro SIREN')))
-            ->add('denomination',TextType::class, array('label'=>'Dénomination sociale', 'attr'=>array('placeholder'=>'Entrez la dénomination sociale')))
+            ->add('siren',TextType::class, ['label'=>'Siren', 'attr'=>['placeholder'=>'Entrez le numéro SIREN']])
+            ->add('denomination',TextType::class, ['label'=>'Dénomination sociale', 'attr'=>['placeholder'=>'Entrez la dénomination sociale']])
             ->add('admin', UserEntrepriseType::class)
-            ->add('Inscription', SubmitType::class, ['attr'=>["class"=>"mt-3 btn-dark btn_list"]])
+            ->add('Inscription', SubmitType::class, ['attr'=>["class"=>"mt-3 btn-warning"]])
         ;
     }
 
